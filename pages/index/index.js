f674893f12cab2eef53fe894ff5175fec193bbb5
wanwan1219 <liwan_1219@126.com>
2018-01-14 17:17:04 +0800

    微信swiper

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+Page({
+  data: {
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    circular: true,
+    dots: true,
+    dotColor:"#ccc",
+    imgUrl: [
+      "../img/banner1.jpg",
+      "../img/banner2.jpg",
+      "../img/banner3.jpg",
+      "../img/banner4.jpg",
+      "../img/banner.jpg"
+    ]
+  }
+})
